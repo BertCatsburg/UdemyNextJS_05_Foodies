dd7bc1e2edb7fb62f5ca8d3b94e8ed6229b47a6e
Bert Catsburg <bert@catsburg.com>
2025-03-11 07:46:07 +0100

    After 122

--- src/lib/meals.ts
+++ src/lib/meals.ts
@@ -1,5 +1,7 @@
 import DB from 'better-sqlite3'
 import {MealsType} from "@/types";
+import slugify from "slugify";
+import xss from 'xss';
 
 export const getMeals= async (): Promise<MealsType[]> => {
     await new Promise((resolve) => setTimeout(resolve, 1000))
@@ -19,6 +21,17 @@ export const getMeal = (slug: string): MealsType => {
     const stmt: DB.Statement = db.prepare('SELECT * FROM meals WHERE slug = ?')
 
     return stmt.get(slug) as MealsType
+}
+
+
+export const saveMeal = (meal: MealsType) => {
+    console.log(meal)
+
+    const slug = slugify(meal.title, {lower: true})  // Creating the Slug based on title
+    const instructions = xss(meal.instructions)  // Sanitize and Clean the instructions field
+
+
+
 }
 
 
